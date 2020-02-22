13c99aafc9ed2577046c2de963ca54d00ed835af
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-22 12:45:45 -0600

    more models

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-  User: require('./user')
+  User: require('./user'),
+  Product: requestAnimationFrame('./product.js')
 };
---
+++ server/models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: String,
+  seller: String,
+  description: String,
+  short_description: String,
+  thumbnail_url: String,
+  img_url: String,
+  flash_day: String,
+  price: Number,
+  post_date: { type: Date, default: Date.now }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
