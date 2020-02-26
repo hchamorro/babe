9dc89af6b10a166bfc46609033ac81ad7ead2f3a
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-25 19:45:56 -0600

    cleaner folders

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/main.css';
 import App from './App';
-import './styles/tailwind.css';
 import * as serviceWorker from './serviceWorker';
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
