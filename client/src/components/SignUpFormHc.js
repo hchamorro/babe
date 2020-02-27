cbed3a5afa2f07098177e66ce6c96d3e2226a8aa
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-27 14:00:56 -0600

    sign up form

---
+++ client/src/components/SignUpFormHc.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../utils/context/UserContextHc';
+
+const SignUpForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassord] = useState('');
+  const [user, setUser] = useContext(UserContext);
+  const updateEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const updatePassord = e => {
+    setPassord(e.target.value);
+  };
+
+  const createUser = e => {
+    e.preventDefault();
+    setUser({ isLoggedIn: true, email: email });
+  };
+  return (
+    <div className="w-full max-w-xs">
+      {console.log('Hey this is what the user is Right Now....', { user })}
+      <form
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        onSubmit={createUser}
+      >
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="email"
+            value={email}
+            onChange={updateEmail}
+          />
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            name="password"
+            value={password}
+            onChange={updatePassord}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            onClick={createUser}
+          >
+            Submit
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Already Member
+          </a>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- client/src/pages/Wellness.js
+++ client/src/pages/Wellness.js
@@ -1,12 +1,13 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../utils/context/UserContextHc';
-
+import SignUpForm from '../components/SignUpFormHc';
 function Wellness() {
   const [user, setUser] = useContext(UserContext);
 
   return (
     <div>
       {user.email}
+      <SignUpForm />
       Hello Wellness
     </div>
   );
