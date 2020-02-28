050198c06bad6c77929e1f59cb8ad12089e995da
Joanna Sowa <joanna.sowa19@gmail.com>
2020-02-27 21:08:06 -0600

    adding background color

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Nav from './components/Nav'
-import Products from './pages/Products';
-import About from './pages/About';
-import Home from './pages/Home';
-import Cart from './pages/Cart';
-import Wellness from './pages/Wellness';
-import News from './pages/News';
-import { UserProvider } from './utils/context/UserContextHc';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Wellness from "./pages/Wellness";
+import News from "./pages/News";
+import { UserProvider } from "./utils/context/UserContextHc";
+import Babe from "./assets/babe.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <UserProvider>
-          <Nav />
-          <Route exact path={['/']} component={Home} />
-          <Route exact path={['/products']} component={Products} />
-          <Route exact path={['/about']} component={About} />
-          <Route exact path={['/cart']} component={Cart} />
-          <Route exact path={['/wellness']} component={Wellness} />
-          <Route exact path={['/news']} component={News} />
-        </UserProvider>
-      </Switch>
-    </Router>
+    <body>
+      <Router>
+        <Switch>
+          <UserProvider>
+            <Nav />
+            <Route exact path={["/"]} component={Home} />
+            <Route exact path={["/products"]} component={Products} />
+            <Route exact path={["/about"]} component={About} />
+            <Route exact path={["/cart"]} component={Cart} />
+            <Route exact path={["/wellness"]} component={Wellness} />
+            <Route exact path={["/news"]} component={News} />
+          </UserProvider>
+        </Switch>
+      </Router>
+    </body>
   );
 }
 
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Babe from "../../assets/babe.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
