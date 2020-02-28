bb42170e64097688dce06b33593b22ee5ff0d0b5
Izzy Edson <isabelle.edson@hyatt.com>
2020-02-27 21:39:14 -0600

    product connecting to database

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,11 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import ProdCard from '../components/ProdCard';
+import API from '../utils/API';
 
 function Products() {
   const [results, setResults] = useState({});
+  const [products, setProducts] = useState([]);
 
-  useEffect(() => {}, []);
 
-  return <div>Hello Products</div>;
+  useEffect(() => {findAllProducts()}, []);
+
+  function findAllProducts() {
+    console.log("i'm clicked!");
+    API.productsAPI
+      .getProductsAPI()
+      .then(res => setProducts(res.data))
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <div>
+      {products.map(product => (
+        <ProdCard 
+          image={product.img_URL[0].img} 
+          title={product.title} 
+          seller={product.seller} 
+          short={product.description} 
+          price={product.price} 
+          key={product.id}/>
+        ))
+      }
+      {console.log('We should see all the proucts here', products)}
+    </div>
+  );
 }
 
 export default Products;
