d122793b52accb0d336c239725920f24b89f40f3
Joanna Sowa <joanna.sowa19@gmail.com>
2020-02-29 12:02:14 -0600

    navbar spacing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,13 +5,25 @@ import Babe from "../../assets/babe.css";
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">B.A.B.E.</Link>
-      <Link to="/About">About</Link>
-      <Link to="/Products">Shop</Link>
-      <Link to="/News">News</Link>
-      <Link to="/Wellness">Wellness</Link>
-      <Link to="/Cart">Cart</Link>
+    <nav className="flex justify-between px-64 py-10">
+      <Link className="font" to="/">
+        B.A.B.E.
+      </Link>
+      <Link className="font" to="/About">
+        About
+      </Link>
+      <Link className="font" to="/Products">
+        Shop
+      </Link>
+      <Link className="font" to="/News">
+        News
+      </Link>
+      <Link className="font" to="/Wellness">
+        Wellness
+      </Link>
+      <Link className="font" to="/Cart">
+        Cart
+      </Link>
     </nav>
   );
 }
