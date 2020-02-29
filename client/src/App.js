c254b1ea9bbb89a71997607b91ddf117e0dfd95f
Hannah Chamorro <hannah.chamorro@gmail.com>
2020-02-29 14:12:51 -0600

    fixed nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Nav from "./components/Nav";
-import Products from "./pages/Products";
-import About from "./pages/About";
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
-import Wellness from "./pages/Wellness";
-import News from "./pages/News";
-import { UserProvider } from "./utils/context/UserContextHc";
-import Babe from "./assets/babe.css";
-import logo from "./assets/BABE-192-logo.png";
-import shoppingCart from "./assets/shopping-cart-solid.svg";
-import hamburger from "./assets/bars-solid.svg";
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Nav from './components/Nav';
+import Products from './pages/Products';
+import About from './pages/About';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Wellness from './pages/Wellness';
+import News from './pages/News';
+import { UserProvider } from './utils/context/UserContextHc';
+import Babe from './assets/babe.css';
+import logo from './assets/BABE-192-logo.png';
+import shoppingCart from './assets/shopping-cart-solid.svg';
+import hamburger from './assets/bars-solid.svg';
 
 function App() {
   return (
@@ -38,14 +38,13 @@ function App() {
             </div>
           </header>
           {/* nav bar */}
-          <Nav>
-            <Route exact path={["/"]} component={Home} />
-            <Route exact path={["/products"]} component={Products} />
-            <Route exact path={["/about"]} component={About} />
-            <Route exact path={["/cart"]} component={Cart} />
-            <Route exact path={["/wellness"]} component={Wellness} />
-            <Route exact path={["/news"]} component={News} />
-          </Nav>
+          <Nav />
+          <Route exact path={['/']} component={Home} />
+          <Route exact path={['/products']} component={Products} />
+          <Route exact path={['/about']} component={About} />
+          <Route exact path={['/cart']} component={Cart} />
+          <Route exact path={['/wellness']} component={Wellness} />
+          <Route exact path={['/news']} component={News} />
         </UserProvider>
       </Switch>
     </Router>
