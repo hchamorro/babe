e3d51b745d7c896fbbd1fd8cf8bcc3112166e1c0
Izzy Edson <isabelle.edson@hyatt.com>
2020-02-29 12:38:17 -0600

    button handler

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ProdCard from '../components/ProdCard';
+import { Link } from "react-router-dom";
 import API from '../utils/API';
 
 function Products() {
@@ -10,23 +11,42 @@ function Products() {
   useEffect(() => {findAllProducts()}, []);
 
   function findAllProducts() {
-    console.log("i'm clicked!");
     API.productsAPI
       .getProductsAPI()
       .then(res => setProducts(res.data))
       .catch(err => console.log(err));
   }
 
+  function AddCart() {
+    //when this button is clicked, take the id of the product you clicked and add it to the product page 
+      console.log('*******Link Clicked*********');
+  }
+
   return (
     <div>
+      {/* <ProdCard >
+      title="Title Placeholder" 
+      seller="Seller Placeholder"
+      short="Short Description Placeholder"
+      price="Price Placeholder"
+      <button onClick={AddCart} className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+      Add to Cart
+    </button>
+    </ProdCard> */}
+
       {products.map(product => (
-        <ProdCard 
-          image={product.img_URL[0].img} 
+        <ProdCard
+          thumbnail={product.thumbnail} 
           title={product.title} 
           seller={product.seller} 
-          short={product.description} 
+          short={product.short_description} 
           price={product.price} 
-          key={product.id}/>
+          AddCart={AddCart}
+          key={product.id}
+          >
+          <Link to="">See More</Link>
+
+        </ProdCard>
         ))
       }
       {console.log('We should see all the proucts here', products)}
