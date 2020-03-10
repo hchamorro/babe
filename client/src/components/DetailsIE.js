e30f8d843a83804c049253f63627911df768feb2
Izzy Edson <isabelle.edson@hyatt.com>
2020-03-10 17:11:31 -0500

    add to cart added on product details

--- client/src/components/DetailsIE.js
+++ client/src/components/DetailsIE.js
@@ -14,7 +14,18 @@ function DetailsIE(props) {
         {/* title & Description column */}
         <div class="w-1/2 h-12">{props.title}</div>
         <div class="w-1/2 h-12">{props.description}</div>
-        <div class="w-1/2 h-12">{props.price}</div>
+        <div class="w-1/2 h-12">${props.price}</div>
+        <div className="flex justify-between">
+          <button
+            onClick={() =>
+              props.AddCart(props.id, props.qty, props.price, props.img)
+            }
+            className="add-to-cart bg-white hover:bg-gray-100 text-gray-800 font-semibold py-0 px-2 border border-gray-400 rounded shadow"
+          >
+            Add to Cart
+          </button>
+          {props.children}
+        </div>
 
     </div>
     )
