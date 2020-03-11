55f6010fc48080096d290baab75452ba4281aeb5
Joanna Sowa <joanna.sowa19@gmail.com>
2020-03-10 21:19:39 -0500

    jo more shitt

--- client/src/components/DetailsIE.js
+++ client/src/components/DetailsIE.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 
 function DetailsIE(props) {
   return (
     <div className="flex mb-4" key={props.id}>
       {/* image column */}
-      <div className="w-1/2 h-12">
+      <div className="w-1/2 h-12 shadow-lg">
         {/* {props.img.map(i => {
                 return <img src={i.img} />
             })} */}
-        <img src={props.img} />
+        <img className="shadow-lg w-auto" src={props.img} />
         {console.log(props.img)}
       </div>
       {/* title & Description column */}
-      <div className="w-1/2 h-12">{props.title}</div>
-      <div className="w-1/2 h-12">{props.description}</div>
-      <div className="w-1/2 h-12">${props.price}</div>
-      <div className="flex justify-between">
-        <button
-          onClick={() =>
-            props.AddCart(props.id, props.qty, props.price, props.img)
-          }
-          className="add-to-cart bg-white hover:bg-gray-100 text-gray-800 font-semibold py-0 px-2 border border-gray-400 rounded shadow"
-        >
-          Add to Cart
-        </button>
-        {props.children}
+      <div className="flex px-4">
+        <div className="item-name px-12">{props.title}</div>
+        <div className="description px-12 w-1/2 h-12">{props.description}</div>
+        <div className="price px-12">${props.price}</div>
+        <div className="px-12">
+          <button
+            onClick={() =>
+              props.AddCart(props.id, props.qty, props.price, props.thumbnail)
+            }
+            className="add-to-cart-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-2 border border-gray-400 rounded shadow"
+          >
+            Add to Cart
+          </button>
+          {props.children}
+        </div>
       </div>
     </div>
   );
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,13 +1,13 @@
-import React, { useState, useContext } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { UserContext } from '../utils/context/UserContextHc';
-import { CartContext } from '../utils/context/CartContextHc';
+import React, { useState, useContext } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { UserContext } from "../utils/context/UserContextHc";
+import { CartContext } from "../utils/context/CartContextHc";
 
-import API from '../utils/API';
+import API from "../utils/API";
 
 const SignUpForm = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassord] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassord] = useState("");
   const [toHome, setToHome] = useState(false);
   const [error, setError] = useState(false);
   const [user, setUser] = useContext(UserContext);
@@ -61,61 +61,63 @@ const SignUpForm = () => {
   return (
     <>
       {toHome ? <Redirect to="/" /> : null}
-      <div className="w-full max-w-xs">
-        <form
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-          onSubmit={createUser}
-        >
-          <h3>Welcome, create your account:</h3>
-          {error ? (
-            <p className="inline-block align-baseline font-bold text-base text-red-500 hover:text-blue-800">
-              * User Name Already Exists
-            </p>
-          ) : null}
-          <div className="mb-6">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              name="email"
-              value={email}
-              onChange={updateEmail}
-            />
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              name="password"
-              value={password}
-              onChange={updatePassord}
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={createUser}
-            >
-              Submit
-            </button>
-            <Link
-              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-              to="/login"
-            >
-              Log In
-            </Link>
-          </div>
-        </form>
+      <div className="px-8 py-8">
+        <div className="w-full max-w-xs">
+          <form
+            className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+            onSubmit={createUser}
+          >
+            <h3>Welcome, create your account:</h3>
+            {error ? (
+              <p className="inline-block align-baseline font-bold text-base text-red-500 hover:text-blue-800">
+                * User Name Already Exists
+              </p>
+            ) : null}
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="email"
+                value={email}
+                onChange={updateEmail}
+              />
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="text"
+                name="password"
+                value={password}
+                onChange={updatePassord}
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={createUser}
+              >
+                Submit
+              </button>
+              <Link
+                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                to="/login"
+              >
+                Log In
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
